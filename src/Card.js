5900f422c78a076962f756601a462d86e1a746c4
Shishir Jha <shishirjhaccr@gmail.com>
2020-02-03 10:09:42 +0530

    added Card Component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='' src='https://robohash.org/shshir?200*200'/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Card;
