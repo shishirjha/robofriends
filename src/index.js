75bd3ce7d7d7533a3b21b1c569835d083f36a927
Shishir Jha <shishirjhaccr@gmail.com>
2020-02-03 10:10:37 +0530

    added object containing hard coded robots

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './Hello.css';
-// import App from './App';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Hello greeting={"Hello"+" React Ninja"}/>, document.getElementById('root'));
+import 'tachyons';
+ReactDOM.render(
+    <div>
+        <Card />
+        <Card />
+        <Card />
+    </div>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
